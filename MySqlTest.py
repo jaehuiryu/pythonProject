fe07db6c265f591b137be414b055b7ea08667599
jaehuiryu <ryumoss@gmail.com>
2022-08-28 11:25:04 +0900

    commit test

--- MySqlTest.py
+++ MySqlTest.py
@@ -1,6 +1,8 @@
 import pymysql
 
-conn = pymysql.connect(host='ryudata.cosnvljfn8l6.ap-northeast-2.rds.amazonaws.com', user='admin', password='8038m011', db='coindata')
+print("TEST STEP 2")
+
+conn = pymysql.connect(host='ryudata.cosnvljfn8l6.ap-northeast-2.rds.amazonaws.com', user='admin', password='캐나다m011', db='coindata')
 
 cur = conn.cursor()
 
